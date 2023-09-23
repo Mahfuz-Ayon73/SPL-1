d7a16a07dff1e40343df005ddf5500b1d8dcd3b4
Mahfuz Ayon <134989125+Mahfuz-Ayon73@users.noreply.github.com>
2023-09-23 19:55:58 +0600

    iterative RNMF

--- RNMF.h
+++ RNMF.h
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 #include "Matrix_Basic_Operations.h"
 
-#define lemda 0.5
+#define lemda 0.01
 
 #define alpha 0.1
 
 #define beta 0.2
 
-#define MAX_ITERATION 500
+#define MAX_ITERATION 100
 
 using namespace std;
 
@@ -15,23 +15,55 @@ vector<vector<double>> update_matrix_U(vector<vector<double>> &original_mat,vect
 {
    vector<vector<double>> U = mat_U , V = mat_V , X = original_mat , V_transpose ,W, D;
  
-   vector<vector<double>> numerator , denominator ,X_Vt , W_U , D_U ,U_Vt ,U_Vt_V , lemda_WU ,lemda_DU;
+   vector<vector<double>> numerator , denominator ,X_V , W_U , D_U ,U_Vt ,U_Vt_V , lemda_WU ,lemda_DU , Ratio , updated_U;
   
    W = adj , D = deg;
 
-   X_Vt = matrixMultiplication(M,N,K,X,V);
+   V_transpose  = transpose(N,K,V);
 
-   W_U = matrixMultiplication(M,M,K,W,U);
+   X_V = matrixMultiplication(M,N,K,X,V);//M*K
+
+  // print(M,K,X_V);
+
+
+   W_U = matrixMultiplication(M,M,K,W,U);//M*K
 
    lemda_WU = constant_multiply(W_U,M,K,lemda);
 
-   numerator = add_elementwise(X_Vt,lemda_WU,M,K);
+   numerator = add_elementwise(X_V,lemda_WU,M,K);//M*K
+
+   //print(M,K,numerator);
+
 
    U_Vt = matrixMultiplication(M,K,N,U,V_transpose);
 
+   cout << "X " << endl;
+
+   //print(M,N,U_Vt);
+
    U_Vt_V = matrixMultiplication(M,N,K,U_Vt,V);
 
-   
+   //print(M,K,U_Vt_V);
+
+   D_U = matrixMultiplication(M,M,K,D,U);
+
+   lemda_DU = constant_multiply(D_U,M,K,lemda);
+
+   //print(M,K,lemda_DU);
+
+   denominator = add_elementwise(U_Vt_V,lemda_DU,M,K);
+
+   //print(M,K,denominator);
+
+   Ratio = divide_elementwise(numerator,denominator,M,K);
+
+   //print(M,K,Ratio);
+
+   updated_U = multiply_elementwise(U,Ratio,M,K);
+
+  // print(M,K,updated_U);
+
+   return updated_U;
 }
 
 vector<vector<double>> update_matrix_V(vector<vector<double>> &original_mat,vector<vector<double>> &mat_U,vector<vector<double>> &mat_V , int N,int K,int M)
@@ -50,25 +82,33 @@ vector<vector<double>> update_matrix_V(vector<vector<double>> &original_mat,vect
 
    numerator = U_transposeX;
 
+   //print(K,M,numerator);
+
    U_transpose_U  = matrixMultiplication(K,M,K,U_transpose,U);
 
    Ut_U_Vt = matrixMultiplication(K,K,N,U_transpose_U,V_transpose);
 
    denominator = Ut_U_Vt;
 
+   //print(K,M,denominator);
+
    Ratio = divide_elementwise(numerator,denominator,K,N);
 
    updated_V = multiply_elementwise(V_transpose,Ratio,K,N);
+
+   return updated_V;
 }
-int main()
+void RNMF()
 {
+   freopen("RNMF_input.txt","r",stdin);
    int M , N , K;
 
    int i,j,k;
 
+   cout << "Enter dimensions:M , N , K\n";
    cin >> M >> N >> K;
 
-   vector<vector<double>> X, U, V ,update_U , update_V ,update_X , adj , deg ;
+   vector<vector<double>> X, U, V ,Vt,update_U , update_V ,update_X , adj , deg ;
 
    double error = 0.0 , min = INT32_MAX;
 
@@ -88,7 +128,7 @@ int main()
 
    deg = Degree_matrix(X,M);
 
-  // initializing A and B randomly
+  // initializing U and V randomly
   for (i = 0; i < M; i++)
   {
 
@@ -126,51 +166,32 @@ int main()
 
    print(N,K,V);
 
- 
-    for(i=1;i<=MAX_ITERATION;i++)
-  {
-
-     if(i % 2 != 0)
+   for(i=1;i<=MAX_ITERATION;i++)
    {
-       U = update_matrix_U(U,V,X,M,K,N);
+     if(i % 2 != 0)
+     {
+        U = update_matrix_U(X,U,V,adj,deg,N,K,M);
 
-       V = transpose(N,K,V);
-       
-       cout << "After Update U and V" << endl;
-       
-       print(M, K, U);
-       print(K, N, V);
-  
-       update_X = matrixMultiplication(M,K,N,U,V);
+        V = transpose(N,K,V);
 
-       print(M,N,update_X);
-  }
- 
-    else{
-      
-      V = transpose(K,N,V);
+        update_X = matrixMultiplication(M,K,N,U,V);
 
-      V = update_matrix_V(U,V,X,M,K,N);
-   
-      V = transpose(K,N,V);
+        print(M,N,update_X);
 
-      cout << "After Update U and V" << endl;
-      print(M, K, U);
-      print(K,N,V);
-  
-      update_X = matrixMultiplication(M,K,N,U,V);
+        V = transpose(K,N,V);
 
-      print(M,N,update_X);
-   
-  }
-  
-   error = error_calculation(X,update_X);
-   cout << "Error:" << error << endl;
-    if(error <= min)
-      min = error;
-  }
+     }
+     else
+     {
 
-  cout << "Factorisation complete!"<<endl;
+        V = update_matrix_V(X,U,V,N,K,M);
 
-  cout << "total iteration:" << i-1 << endl << "Minimum Error: " << min << endl;
+        update_X = matrixMultiplication(M,K,N,U,V);
+
+         print(M,N,update_X);
+        
+        V = transpose(K,N,V);
+     }
+
+   }
 }
